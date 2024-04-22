a46b674289d00bab4970b82980460ac12b10f4d2
karthikrv55 <167761943+karthikrv55@users.noreply.github.com>
2024-04-22 15:46:30 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 
 TEST(StringCalculatorTestSuite,GivenEmptyStringZeroIsExpected){
-    //Arrange
+    //Arrangee
     StringCalculator objUnderTest;
     string input="";
     int expectedValue=0;
