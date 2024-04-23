ddda2d6d08e0841b7d4c8cbec74a4fd24bd41c99
karthikrv55 <167761943+karthikrv55@users.noreply.github.com>
2024-04-23 12:17:40 +0530

    Update TeleCo.h

--- TeleCo.h
+++ TeleCo.h
@@ -1,11 +1,11 @@
-#include <string>
+e#include <string>
 #include <vector>
 #include <numeric>
 #include <regex>
 
 using namespace std;
 
-class ColorPairIn {
+class ColorPairIn extends ColorPair{
 public:
 	 ColorPair GetColorFromPairNumber(int pairNumber);
   int GetPairNumberFromColor(MajorColor major, MinorColor minor);
